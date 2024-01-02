2f2f87351f81b0e35b001afbdb49256475a33b68
Denys Niestierov <niestierov.denys.natife@gmail.com>
2024-01-02 10:42:16 +0200

    added json decoder extension

--- NiMovies/Common/Extensions/JSONDecoder+Default.swift
+++ NiMovies/Common/Extensions/JSONDecoder+Default.swift
@@ -1,8 +1,16 @@
 //
-//  JSONDeconder+Default.swift
+//  JSONDecoder+Default.swift
 //  NiMovies
 //
 //  Created by Denys Niestierov on 02.01.2024.
 //
 
 import Foundation
+
+extension JSONDecoder {
+    static var `default`: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+}
