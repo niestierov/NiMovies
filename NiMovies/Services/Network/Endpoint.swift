66449e05b0b68681a604d7aab5387d657e98553b
Denys Niestierov <niestierov.denys.natife@gmail.com>
2024-01-02 10:41:47 +0200

    added endpoint

--- NiMovies/Services/Network/Endpoint.swift
+++ NiMovies/Services/Network/Endpoint.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Alamofire
+
+class Endpoint<T: Decodable>: Requestable {
+    typealias ResponseType = T
+    
+    // MARK: - Properties -
+    
+    let url: URL?
+    let parameters: [String: Any]
+    let method: HTTPMethod
+    let encoding: ParameterEncoding
+    
+    // MARK: - Init -
+    
+    init(
+        url: URL?,
+        parameters: [String: Any] = [:],
+        method: HTTPMethod,
+        encoding: ParameterEncoding = URLEncoding.default
+    ) {
+        self.url = url
+        self.parameters = parameters
+        self.method = method
+        self.encoding = encoding
+    }
+}
