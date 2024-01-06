ebdb4c372138e6ca153e3ac6b22a5ff830d43bf9
Denys Niestierov <niestierov.denys.natife@gmail.com>
2024-01-06 13:26:16 +0200

    added movie list api service

---
+++ NiMovies/Presentation/Scenes/MovieList/ApiService/MovieConfiguration.swift
@@ -0,0 +1,19 @@
+//
+//  MovieConfiguration.swift
+//  NiMovies
+//
+//  Created by Denys Niestierov on 02.01.2024.
+//
+
+import Foundation
+
+struct MovieConfiguration {
+    static let baseUrl = "https://api.themoviedb.org/3"
+    static let basePosterUrl = "https://image.tmdb.org/t/p/w500"
+    static let apiSecretKey = "API_KEY_THEMOVIEDB"
+    static let apiKey = "api_key"
+    
+    static func getSecretKey() -> String {
+        Bundle.main.object(forInfoDictionaryKey: apiSecretKey) as? String ?? ""
+    }
+}
